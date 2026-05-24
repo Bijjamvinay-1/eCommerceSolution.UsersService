9bd705b86bed3185e0a6ddc29c472c19600976ce
Bijjamvinay-1 <vinaybijjam7771@gmail.com>
2026-05-24 14:26:38 +0530

    FluentValidation For Login and request DTOs - Ratern using Data Annotations

--- eCommerce.Core/DependencyInjection.cs
+++ eCommerce.Core/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using eCommerce.Core.ServiceContracts;
 using eCommerce.Core.Services;
+using eCommerce.Core.Validators;
+using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace eCommerce.Core;
@@ -17,6 +19,7 @@ public static class DependencyInjection
         //Core services often include data access, caching and other low-level components.
 
         services.AddTransient<IUsersService, UsersService>();
+        services.AddValidatorsFromAssemblyContaining<LoginRequestValidator>();
         return services;
     }
 }
--- eCommerceAPI/Program.cs
+++ eCommerceAPI/Program.cs
@@ -3,6 +3,8 @@ using eCommerce.Core;
 using eCommerce.API.Middlewares;
 using System.Text.Json.Serialization;
 using eCommerce.Core.Mappers;
+using FluentValidation.AspNetCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,9 @@ builder.Services.AddControllers().AddJsonOptions(options => {
 });
 
 builder.Services.AddAutoMapper(cfg => cfg.AddMaps(typeof(ApplicationUserMappingProfile).Assembly));
+
+//FluentValidations
+builder.Services.AddFluentValidationAutoValidation();
 
 //Build the web application
 var app = builder.Build();
